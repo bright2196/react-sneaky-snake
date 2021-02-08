b4cc10303458d3c142c442fa40ff042bc79a29eb
Bright2196 <claireriussetcr@gmail.com>
2021-02-08 15:37:18 +0100

    config

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sneaky-Snake-React-Native</Text>
+      <Text>Le jeu</Text>
       <StatusBar style="auto" />
     </View>
   );
